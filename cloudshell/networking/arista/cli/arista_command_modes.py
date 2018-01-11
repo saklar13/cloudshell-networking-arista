b14fc0a2f2ac6e0dcb49da4ff5f315f3a2b61987
Kyrylo Maksymenko <saklar13@gmail.com>
2018-01-11 13:52:36 +0200

    fix using enable password

--- cloudshell/networking/arista/cli/arista_command_modes.py
+++ cloudshell/networking/arista/cli/arista_command_modes.py
@@ -1,3 +1,6 @@
+import re
+import time
+
 from cloudshell.cli.command_mode import CommandMode
 
 
@@ -12,10 +15,16 @@ class AristaDefaultCommandMode(CommandMode):
 
         :param resource_config:
         """
+
         self.resource_config = resource_config
         self._api = api
-        CommandMode.__init__(self, AristaDefaultCommandMode.PROMPT, AristaDefaultCommandMode.ENTER_COMMAND,
-                             AristaDefaultCommandMode.EXIT_COMMAND)
+
+        CommandMode.__init__(
+            self,
+            AristaDefaultCommandMode.PROMPT,
+            AristaDefaultCommandMode.ENTER_COMMAND,
+            AristaDefaultCommandMode.EXIT_COMMAND,
+        )
 
 
 class AristaEnableCommandMode(CommandMode):
@@ -29,14 +38,36 @@ class AristaEnableCommandMode(CommandMode):
 
         :param resource_config:
         """
+
         self.resource_config = resource_config
         self._api = api
+        self._enable_password = None
 
-        CommandMode.__init__(self, AristaEnableCommandMode.PROMPT, AristaEnableCommandMode.ENTER_COMMAND,
-                             AristaEnableCommandMode.EXIT_COMMAND)
+        CommandMode.__init__(
+            self,
+            AristaEnableCommandMode.PROMPT,
+            AristaEnableCommandMode.ENTER_COMMAND,
+            AristaEnableCommandMode.EXIT_COMMAND,
+            enter_action_map=self.enter_action_map(),
+        )
+
+    @property
+    def enable_password(self):
+        if not self._enable_password:
+            password = self.resource_config.enable_password
+            self._enable_password = self._api.DecryptPassword(password).Value
+        return self._enable_password
+
+    def enter_action_map(self):
+        return {
+            "[Pp]assword":
+                lambda session, logger: session.send_line(self.enable_password, logger)
+        }
 
 
 class AristaConfigCommandMode(CommandMode):
+    MAX_ENTER_CONFIG_MODE_RETRIES = 5
+    ENTER_CONFIG_RETRY_TIMEOUT = 5
     PROMPT = r'\(config.*\)#\s*$'
     ENTER_COMMAND = 'configure terminal'
     EXIT_COMMAND = 'end'
@@ -50,9 +81,34 @@ class AristaConfigCommandMode(CommandMode):
         self.resource_config = resource_config
         self._api = api
 
-        CommandMode.__init__(self, AristaConfigCommandMode.PROMPT,
-                             AristaConfigCommandMode.ENTER_COMMAND,
-                             AristaConfigCommandMode.EXIT_COMMAND)
+        CommandMode.__init__(
+            self,
+            AristaConfigCommandMode.PROMPT,
+            AristaConfigCommandMode.ENTER_COMMAND,
+            AristaConfigCommandMode.EXIT_COMMAND,
+
+        )
+
+    def enter_action_map(self):
+        return {r"{}.*$".format(AristaEnableCommandMode.PROMPT): self._check_config_mode}
+
+    def _check_config_mode(self, session, logger):
+        error_message = "Failed to enter config mode, please check logs, for details"
+        conf_prompt = AristaConfigCommandMode.PROMPT
+        enable_prompt = AristaEnableCommandMode.PROMPT
+
+        retry = 0
+        output = session.hardware_expect("", "{0}|{1}".format(conf_prompt, enable_prompt), logger)
+        while not re.search(conf_prompt, output) and retry < self.MAX_ENTER_CONFIG_MODE_RETRIES:
+            output = session.hardware_expect(
+                AristaConfigCommandMode.ENTER_COMMAND,
+                "{0}|{1}".format(enable_prompt, conf_prompt),
+                logger)
+            time.sleep(self.ENTER_CONFIG_RETRY_TIMEOUT)
+            retry += 1
+
+        if not re.search(conf_prompt, output):
+            raise Exception(error_message)
 
 
 CommandMode.RELATIONS_DICT = {
